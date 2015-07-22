9837a570d08b5f3fdda4fdc9d695836cb30b03bd
Ursin Brunner <ursin.brunner@gmail.com>
2015-07-22 14:32:48 +0200

    show answered questions (including answers) when selecting a patient in the anteroom

--- app/partial-views/virtual-anteroom/virtual-anteroom.js
+++ app/partial-views/virtual-anteroom/virtual-anteroom.js
@@ -11,10 +11,15 @@ angular.module('myApp.virtualAnteroom', ['ngRoute'])
   .controller('VirtualAnteroomCtrl',['$scope','Patient', function ($scope, Patient) {
     $scope.patients = Patient.query();
     $scope.newPatient = new Patient();
+    $scope.selectedPatient = null;
 
     $scope.deletePatient = function (patientToDelete) {
       patientToDelete.$remove({patientId:patientToDelete.Id});
       $scope.patients.splice($scope.patients.indexOf(patientToDelete),1);
+
+      if(patientToDelete == $scope.selectedPatient) {
+        $scope.selectedPatient = null;
+      }
     };
 
     $scope.createNewPatient = function () {
@@ -22,5 +27,23 @@ angular.module('myApp.virtualAnteroom', ['ngRoute'])
       $scope.patients.push($scope.newPatient);
       $scope.newPatient = new Patient();
     };
+
+    $scope.selectPatient = function (patientToSelect) {
+      $scope.selectedPatient = patientToSelect;
+      //this is a little helper-method, because the service returns the answered-questions as a hash (dictionary, map, whatever), but we need only the values.
+      // This way, we get an array with only values.
+      $scope.selectedPatient.getQuestionsWithAnswerValues = function () {
+        var keys = Object.keys($scope.selectedPatient.QuestionsWithAnswer);
+        return keys.map(function(v) { return $scope.selectedPatient.QuestionsWithAnswer[v]; });
+      }
+    };
+
+    $scope.getSelectPatientPersonInformation = function () {
+      if($scope.selectedPatient != null){
+        return 'von Patient ' + $scope.selectedPatient.Name + ', Alter: ' + $scope.selectedPatient.Age + ' Hauptbeschwerde: ' + $scope.selectedPatient.ChiefComplaint;
+      }
+    }
+
+
   }]);
 
